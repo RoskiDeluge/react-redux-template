8d517f3b72b64f6a4947c489f77368f1866d6533
Michael Nunes <tinysquid.git@gmail.com>
2020-02-27 22:13:41 -0800

    Add test for DemoPage

---
+++ src/__tests__/DemoPage/DemoPage.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import DemoPage from "../../pages/DemoPage";
+import DemoComponent from "../../components/DemoPage/DemoComponent";
+
+describe("<DemoPage>", () => {
+	let wrapper;
+
+	beforeEach(() => {
+		wrapper = shallow(<DemoPage />);
+	});
+
+	test("renders <DemoComponent> as a child component", () => {
+		expect(wrapper.exists(DemoComponent)).toBe(true);
+	});
+});
