332e76edb78e9df5cc4c0e3732f25276da7ff01c
Michael Nunes <tinysquid.git@gmail.com>
2020-02-27 22:43:53 -0800

    Add routing test to App

--- src/__tests__/App/App.test.js
+++ src/__tests__/App/App.test.js
@@ -1,15 +1,35 @@
 import React from "react";
-import { shallow } from "enzyme";
+
+import { mount } from "enzyme";
 import { MemoryRouter } from "react-router-dom";
 
-import { App } from "../../App";
+// Mock redux store
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import App from "../../App";
+import DemoPage from "../../pages/DemoPage";
+
+const mockStore = configureStore({});
 
 describe("<App>", () => {
-	test("renders self without error", () => {
-		shallow(
-			<MemoryRouter>
-				<App />
-			</MemoryRouter>
+	let store;
+	let wrapper;
+
+	beforeEach(() => {
+		store = mockStore({
+			Demo: {},
+		});
+	});
+
+	test("renders <DemoPage> at correct route", () => {
+		wrapper = mount(
+			<Provider store={store}>
+				<MemoryRouter initialEntries={["/"]}>
+					<App />
+				</MemoryRouter>
+			</Provider>
 		);
+		expect(wrapper.exists(DemoPage)).toBe(true);
 	});
 });
